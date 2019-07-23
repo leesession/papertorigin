a3a8c28ae699f6d6314a782ee0882ac29a23b971
wuxuanfeng <wuxuanfeng2007@163.com>
2019-07-23 10:51:10 +0800

    111

--- src/router.js
+++ src/router.js
@@ -1,25 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import home from './views/home/home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: home
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    // {
+      // path: '/about',
+      // name: 'about',
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
---
+++ vue.config.js
@@ -0,0 +1,52 @@
+// api地址
+const api = 'http://192.168.0.234:8083';
+//引入webpack
+const webpack = require('webpack');
+
+module.exports = {
+  // 部署应用时的基本 URL
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  // 打包后生成的文件目录名称
+  outputDir: "dist",
+  // 打包后静态文件的目录名称
+  assetsDir: "static",
+  // 开发环境配置（它支持webPack-dev-server的所有选项）
+  devServer: {
+    host: "localhost",
+    port: 8080, 
+    https: false, 
+    open: true, //配置自动启动浏览器
+    // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+    // 配置多个代理
+    proxy: {
+      "/financial": {
+        target: api,
+        ws: false,
+        changeOrigin: true,   //是否跨域
+        pathRewrite: {
+          '^/financial': '/financial'
+        }
+      }    
+    }
+  },
+  // 插件
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery'
+      })
+    ]
+  },
+	
+	pages: {
+		index: {
+			entry: 'src/main.js',
+			template: 'public/index.html',
+			filename: 'index.html',
+			title: 'Index Page',
+			chunks: ['chunk-vendors', 'chunk-common', 'index']
+		}
+	}
+	
+};
