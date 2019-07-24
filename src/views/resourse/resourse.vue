09c528e3e39920439f6718c4c200773f6d90d74d
wuxuanfeng <wuxuanfeng2007@163.com>
2019-07-24 18:31:32 +0800

    111

--- src/views/resourse/resourse.vue
+++ src/views/resourse/resourse.vue
@@ -2,8 +2,13 @@
   <div class="resourse">
     <site-header></site-header>
     <search title="Our Resourse" index="2"></search>
-    <h1>What resources do we have?</h1>
-    <h6>TASSEL SCHOLAR make cooperation with below journal publishers to provide academic paper information searching and retrieving</h6>
+    <div class="title wrap1300">
+      <h1>What resources do we have?</h1>
+      <h6>TASSEL SCHOLAR make cooperation with below journal publishers to provide academic paper information searching and retrieving</h6>
+    </div>
+    <div class="box wrap1300">
+      
+    </div>
   </div>
 </template>
 
@@ -22,6 +27,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.title{
+  h1{
+    height: 92px;
+    line-height: 110px;
+    font-size: 28px;
+    color: #29697E;
+  }
+  p{
+    font-size: 18px;
+    color: #2D3F45;
+  }
+}
+.box{
+  width: 
+}
 
 </style>
 
